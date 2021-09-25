2191335ef058a72f664e0ede2584a9ce64faec6c
Tee <hvtee@yahoo.com.hk>
2021-09-25 19:26:22 +0800

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -13,7 +13,7 @@ from utils_functions import find_postal, find_nearest, dist_from_location, map, 
 import streamlit.components.v1 as components
 import pydeck as pdk
 from pathlib import Path
-import joblib
+#import joblib
 
 
 _max_width_()
